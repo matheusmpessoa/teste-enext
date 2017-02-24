78f0ca3ad25746d56618b4c6866f1706c2368885
matheusmpessoa <matheusmpessoa17@gmail.com>
2017-02-24 11:44:49 -0300

    Header/Nav finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-/* activate scrollspy menu */
-$('body').scrollspy({
-    target: '#navbar-collapsible',
-    offset: 52
-});
-
 /* Fechar menu navbar-collapse */
 $('.navbar-collapse a').click(function () {
     $(".navbar-collapse").collapse('hide');
